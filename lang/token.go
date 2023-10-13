ec81aa33ca13f844f607fa93f975f8fc9735e03a
Larry Skuse <larry.skuse@gmail.com>
2023-10-13 06:18:24 -0400

    Flatten project structure

---
+++ lang/token.go
@@ -0,0 +1,63 @@
+package lang
+
+import "fmt"
+
+type TokenType int
+
+const (
+	// single char tokens
+	tokenTypeLeftParen TokenType = iota
+	tokenTypeRightParen
+	tokenTypeLeftBrace
+	tokenTypeRightBrace
+	tokenTypeComma
+	tokenTypeDot
+	tokenTypeMinus
+	tokenTypePlus
+	tokenTypeSemicolon
+	tokenTypeSlash
+	tokenTypeStar
+	// comparison operator tokens
+	tokenTypeBang
+	tokenTypeBangEqual
+	tokenTypeEqual
+	tokenTypeEqualEqual
+	tokenTypeGreater
+	tokenTypeGreaterEqual
+	tokenTypeLess
+	tokenTypeLessEqual
+	// literals
+	tokenTypeIdentifier
+	tokenTypeString
+	tokenTypeNumber
+	// keywords
+	tokenTypeAnd
+	tokenTypeClass
+	tokenTypeElse
+	tokenTypeFalse
+	tokenTypeFun
+	tokenTypeFor
+	tokenTypeIf
+	tokenTypeNil
+	tokenTypeOr
+	tokenTypePrint
+	tokenTypeReturn
+	tokenTypeSuper
+	tokenTypeThis
+	tokenTypeTrue
+	tokenTypeVar
+	tokenTypeWhile
+	// end of file
+	tokenTypeEndOfFile
+)
+
+type Token struct {
+	tokenType TokenType
+	lexeme    string
+	literal   any
+	line      int
+}
+
+func (t Token) ToString() string {
+	return fmt.Sprintf("%d %s %s", t.tokenType, t.lexeme, t.literal)
+}
